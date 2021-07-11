198ba8b98dac274b31e98d341d0b24ea00804532
Baldur Bjarnason <baldur@rebus.foundation>
2021-07-11 21:20:09 +0000

    Markup purify

---
+++ src/dompurify.js
@@ -0,0 +1,95 @@
+import { css } from "./css.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const validDataUrl = require("valid-data-url");
+
+const purifyConfig = {
+  KEEP_CONTENT: false,
+  IN_PLACE: true,
+  WHOLE_DOCUMENT: true,
+  ADD_TAGS: ["link", "ink-page"],
+  FORBID_TAGS: ["meta", "form"],
+  FORBID_ATTR: ["srcset", "action", "background", "poster"],
+};
+
+// In theory this should work for SVG images as well.
+export async function purify(chapter, chapterPath, contentType = "text/html") {
+  const resourceURL = new URL(chapterPath, "https://example.com/");
+  let dom;
+  try {
+    dom = new JSDOM(chapter, {
+      contentType,
+      url: "http://localhost",
+    });
+  } catch (err) {
+    dom = new JSDOM(chapter, {
+      contentType: "text/html",
+      url: "http://localhost",
+    });
+  }
+  const window = dom.window;
+  for (const node of window.document.querySelectorAll("[style]")) {
+    try {
+      const styles = await css(
+        `body {${node.getAttribute("style")}}`,
+        resourceURL.href
+      );
+      node.setAttribute("style", styles.split(/{|}/)[1]);
+    } catch (err) {
+      node.removeAttribute("style");
+    }
+  }
+  for (const node of window.document.querySelectorAll("style")) {
+    try {
+      node.textContent = await css(node.textContent, resourceURL.href);
+    } catch (err) {
+      node.textContent = "";
+    }
+  }
+  const DOMPurify = createDOMPurify(window);
+  // Based on sample from https://github.com/cure53/DOMPurify/tree/master/demos, same license as DOMPurify
+
+  DOMPurify.addHook("uponSanitizeElement", function (node, data) {
+    if (
+      node.getAttributeNS &&
+      node.getAttributeNS("http://www.idpf.org/2007/ops", "type")
+    ) {
+      node.dataset.epubType = node.getAttributeNS(
+        "http://www.idpf.org/2007/ops",
+        "type"
+      );
+    }
+  });
+  DOMPurify.addHook("afterSanitizeAttributes", function (node) {
+    // All src urls must be relative. This will have to be improved once we start expanding our format support
+    if (
+      node.hasAttribute("src") &&
+      !testPath(node.getAttribute("src"), resourceURL) &&
+      !validDataUrl(node.getAttribute("src"))
+    ) {
+      node.remove();
+    }
+    if (
+      node.tagName.toLowerCase() === "link" &&
+      node.hasAttribute("href") &&
+      node.getAttribute("rel") === "stylesheet" &&
+      !testPath(node.getAttribute("href"), resourceURL)
+    ) {
+      node.remove();
+    }
+  });
+  const clean = DOMPurify.sanitize(
+    window.document.documentElement,
+    purifyConfig
+  );
+  return dom.serialize(clean);
+}
+
+function testPath(path, resourceURL) {
+  const base = new URL(resourceURL, "http://example.com");
+  const url = new URL(path, base);
+  // If the hostname doesn't equal that of the base URL we provided, then it is a full URL and so not supported
+  return url.hostname === base.hostname;
+}
---
+++ tap-snapshots/tests/dompurify.test.js.test.cjs
@@ -0,0 +1,44 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`tests/dompurify.test.js TAP dompurify - html > dompurify html 1`] = `
+<html><head>
+    <style>#pub-soup soup-body { background-color: red;}</style>
+    </head><body>
+    <h1>Test</h1>
+    </body></html>
+`
+
+exports[`tests/dompurify.test.js TAP dompurify - invalid xhtml > dompurify invalid xhtml 1`] = `
+<!--?xml version="1.0" encoding="utf-8"?--><!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/xhtml"><head>
+    <style>#pub-soup soup-body { background-color: red;}</style>
+    </head><body>
+    <br>
+    <h1>Test</h1>
+    <p><img src="/image.png"></p>
+    </body></html>
+`
+
+exports[`tests/dompurify.test.js TAP dompurify - svg > dompurify svg 1`] = `
+<svg xmlns="http://www.w3.org/2000/svg" height="20" width="38" style="background-color: red;"><path fill="#000" fill-rule="nonzero" d="M0 10c0 .546.414.983.932.983h33.887l-6.954 7.337a1.015 1.015 0 0 0 0 1.39.892.892 0 0 0 1.317 0l8.545-9.015a1.023 1.023 0 0 0 0-1.39L29.182.29a.892.892 0 0 0-1.317 0 1.015 1.015 0 0 0 0 1.39l6.954 7.337H.932C.414 9.017 0 9.454 0 10z"/></svg>
+`
+
+exports[`tests/dompurify.test.js TAP dompurify - xhtml > dompurify xhtml 1`] = `
+<!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <link href="safe.css" rel="stylesheet" />
+    
+    <style></style>
+    </head><body>
+    <h1>Test</h1>
+    <p data-epub-type="appendix"><a href="/relative/url">test</a><a href="http://example.com">test2</a></p>
+    <p></p>
+    <p><img src="/image.png" /></p>
+    <p><img src="image.png" /></p>
+    <p><img src="data:image/svg+xml;charset=utf-8,%3Csvg%20width%3D%2238%22%20height%3D%2220%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22M0%2010c0%20.546.414.983.932.983h33.887l-6.954%207.337a1.015%201.015%200%200%200%200%201.39.892.892%200%200%200%201.317%200l8.545-9.015a1.023%201.023%200%200%200%200-1.39L29.182.29a.892.892%200%200%200-1.317%200%201.015%201.015%200%200%200%200%201.39l6.954%207.337H.932C.414%209.017%200%209.454%200%2010z%22%20fill-rule%3D%22nonzero%22%20fill%3D%22%23000%22%2F%3E%3C%2Fsvg%3E" /></p>
+    </body></html>
+`
