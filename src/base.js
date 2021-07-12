437781e8b6857316339b638a5eb90410519287f3
Baldur Bjarnason <baldur@rebus.foundation>
2021-07-12 16:14:33 +0000

    Base handles urls and ids

--- src/base.js
+++ src/base.js
@@ -1,19 +1,69 @@
+import { Names } from "./names.js";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const srcset = require("srcset");
+
 export class Base {
-  constructor({ media, link, style, base } = {}) {
+  constructor({ media, link, style, base } = {}, { names = new Names() } = {}) {
     base = base || "http://www.example.com/";
-    this.base = {
+    this.url = {
       base: base,
       media: media || base,
       link: link || base,
       style: style || base,
     };
+    this.names = names;
+  }
+
+  // What this does is return a full zip path if the path refers to an in-archive file or null if it does not
+  full(path, fileBase) {
+    const baseURL = new URL(fileBase, "https://www.example.com/");
+    const absoluteURL = new URL(path, baseURL);
+    if (absoluteURL.host !== "www.example.com") {
+      return null;
+    } else {
+      return absoluteURL.pathname.replace("/", "");
+    }
+  }
+
+  transform(path, fileBase, type) {
+    const full = this.full(path, fileBase);
+    let id;
+    if (full && type === "link") {
+      id = this.names.get(full);
+      // We ignore query parameters for internal references.
+      const hash = new URL(
+        path,
+        new URL(fileBase, "https://www.example.com/")
+      ).hash.replace("#", "");
+      return hash ? `#${id}:${hash}` : `#${id}`;
+    } else if (full && this[type]) {
+      id = this.names.get(full);
+      return this[type](id);
+    } else {
+      // This isn't a url for something in the archive so we just return it as is.
+      return path;
+    }
+  }
+
+  srcset(set, fileBase) {
+    const parsed = srcset.parse(set);
+    for (const src of parsed) {
+      src.url = this.transform(src.url, fileBase, "media");
+    }
+    return srcset.stringify(parsed);
+  }
+
+  id(hash, fileBase) {
+    return `${this.names.get(fileBase)}:${hash}`;
   }
 
   href(url) {
-    return this.serialize(new URL(url, this.base.base));
+    return this.serialize(new URL(url, this.url.base));
   }
 
-  serialize(url = new URL(this.base.base)) {
+  serialize(url = new URL(this.url.base)) {
     if (url.hostname === "www.example.com") {
       return url.pathname;
     } else {
@@ -22,14 +72,14 @@ export class Base {
   }
 
   media(url) {
-    return this.serialize(new URL(url, this.base.media));
+    return this.serialize(new URL(url, this.url.media));
   }
 
   link(url) {
-    return this.serialize(new URL(url, this.base.link));
+    return this.serialize(new URL(url, this.url.link));
   }
 
   style(url) {
-    return this.serialize(new URL(url, this.base.style));
+    return this.serialize(new URL(url, this.url.style));
   }
 }
--- tests/base.test.js
+++ tests/base.test.js
@@ -1,4 +1,5 @@
 import { Base } from "../src/base.js";
+import { Names } from "../src/names.js";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const tap = require("tap");
@@ -22,4 +23,120 @@ tap.test("base", async (test) => {
   test.equal(base.media("/test"), "http://media.example.com/test");
   test.equal(base.link("/test"), "http://link.example.com/test");
   test.equal(base.style("/test"), "http://style.example.com/test");
+});
+
+tap.test("base - full", async (test) => {
+  const base = new Base({
+    base: "http://test.example.com/",
+    media: "http://media.example.com/",
+    link: "http://link.example.com/",
+    style: "http://style.example.com/",
+  });
+  test.equal(
+    base.full("relative/path/src.img", "chapter/path.xhtml"),
+    "chapter/relative/path/src.img"
+  );
+  test.equal(
+    base.full("http://img.example.com/path/src.img", "chapter/path.xhtml"),
+    null
+  );
+});
+
+tap.test("base - id", async (test) => {
+  let counter = 0;
+
+  function id() {
+    counter = counter + 1;
+    return counter + "_id";
+  }
+
+  const names = new Names(id);
+  const base = new Base(
+    {
+      base: "http://test.example.com/",
+      media: "http://media.example.com/",
+      link: "http://link.example.com/",
+      style: "http://style.example.com/",
+    },
+    { names }
+  );
+  test.equal(base.id("heading", "chapter/path.xhtml"), "1_id:heading");
+});
+tap.test("base - srcset", async (test) => {
+  let counter = 0;
+
+  function id() {
+    counter = counter + 1;
+    return counter + "_id";
+  }
+
+  const names = new Names(id);
+  const base = new Base(
+    {
+      base: "http://test.example.com/",
+      media: "http://media.example.com/",
+      link: "http://link.example.com/",
+      style: "http://style.example.com/",
+    },
+    { names }
+  );
+  test.equal(
+    base.srcset(
+      "images/Moby-Dick_FE_title_page-hd.jpg 2x, images/Moby-Dick_FE_title_page-phone.jpg 100w, images/Moby-Dick_FE_title_page-phone-HD.jpg 100w 2x",
+      "chapter/path.xhtml"
+    ),
+    "http://media.example.com/1_id 2x, http://media.example.com/2_id 100w, http://media.example.com/3_id 100w 2x"
+  );
+});
+
+tap.test("base - transform", async (test) => {
+  let counter = 0;
+
+  function id() {
+    counter = counter + 1;
+    return counter + "_id";
+  }
+
+  const names = new Names(id);
+  const base = new Base(
+    {
+      base: "http://test.example.com/",
+      media: "http://media.example.com/",
+      link: "http://link.example.com/",
+      style: "http://style.example.com/",
+    },
+    { names }
+  );
+  test.equal(
+    base.transform("path/subchapter.xhtml", "chapter/path.xhtml", "link"),
+    "#1_id"
+  );
+  test.equal(
+    base.transform(
+      "path/subchapter.xhtml#heading",
+      "chapter/path.xhtml",
+      "link"
+    ),
+    "#1_id:heading"
+  );
+  test.equal(
+    base.transform("relative/path/src.img", "chapter/path.xhtml", "media"),
+    "http://media.example.com/2_id"
+  );
+  test.equal(
+    base.transform("relative/path/src.css", "chapter/path.xhtml", "style"),
+    "http://style.example.com/3_id"
+  );
+  test.equal(
+    base.transform("http://img.example.com/path/src.img", "chapter/path.xhtml"),
+    "http://img.example.com/path/src.img"
+  );
+  test.equal(
+    base.transform(
+      "../chapter1/path.xhtml#heading",
+      "chapter/path.xhtml",
+      "link"
+    ),
+    "#4_id:heading"
+  );
 });
