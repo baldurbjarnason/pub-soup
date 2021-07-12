7e01b1fc62ad941ad9b4c965ec11f0a68c833fe5
Baldur Bjarnason <baldur@rebus.foundation>
2021-07-12 15:46:07 +0000

    env module update

--- src/env.js
+++ src/env.js
@@ -1,7 +1,8 @@
+import { Names } from "./names.js";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const nanoid = require("nanoid");
 const unzipper = require("unzipper");
 const request = require("request");
 
-export const env = { nanoid, unzipper, request };
+export const env = { nanoid, unzipper, request, names: new Names(nanoid) };
